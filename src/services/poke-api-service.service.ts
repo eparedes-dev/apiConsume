e15c4335480d97cb02d79437b0200b6a68bc1674
eparedes-dev <erick.paredes.nunez@udla.edu.ec>
2023-12-13 17:42:10 -0500

    Creación de cadena de conexión con PokeApi

--- src/services/poke-api-service.service.ts
+++ src/services/poke-api-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiServiceService {
+  private apiUrl = 'https://pokeapi.co/api/v2/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getpokemonList(): Observable<any>{
+    return this.http.get(`${this.apiUrl}pokemon)`);
+  }
+
+  getpokemonDetails(id: number): Observable<any>{
+    return this.http.get(`${this.apiUrl}pokemon/${id}`);
+  }
+
 }
